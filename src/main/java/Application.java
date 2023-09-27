dc35d8974d2aaa1c55c73edcce8692cc57784043
jose <2409jmsousa@gmail.com>
2023-09-27 09:22:33 +0100

    step 4

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,5 +1,10 @@
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Terminal terminal = new
+                DefaultTerminalFactory().createTerminal();
+        Screen screen = new TerminalScreen(terminal);
+        screen.setCursorPosition(null); // we don't need a cursor
+        screen.startScreen(); // screens must be started
+        screen.doResizeIfNecessary(); // resize screen if necessary
     }
 }
