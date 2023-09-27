fba763eb128f11280241c2c5d2c5723453551c27
jose <2409jmsousa@gmail.com>
2023-09-27 10:10:41 +0100

    step 7

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,5 +1,7 @@
 import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
@@ -8,6 +10,8 @@ import com.googlecode.lanterna.terminal.Terminal;
 import java.io.IOException;
 
 public class Game {
+    private int x = 20;
+    private int y = 10;
     private Screen screen;
     public static void main(String[] args) throws IOException {
         Game game = new Game();
@@ -25,18 +29,58 @@ public class Game {
         screen.setCursorPosition(null); // we don't need a cursor
         screen.startScreen(); // screens must be started
         screen.doResizeIfNecessary();
+
     }
 
     private void draw() throws IOException {
         screen.clear();
-        screen.setCharacter(10, 10, TextCharacter.fromCharacter('X')
+        screen.setCharacter(x, y, TextCharacter.fromCharacter('X')
                 [0]);
         screen.refresh();
 
 
     }
+    private boolean  processKey(KeyStroke key) throws IOException {
+        System.out.println(key);
+        boolean res= true;
+        if (key.getKeyType() == KeyType.EOF){
+            res=false;
+
+        }
+
+        if (key.getKeyType() == KeyType.ArrowUp){
+            y-=1;
+
+        }
+        if (key.getKeyType() == KeyType.ArrowDown){
+            y+=1;
+        }
+        if (key.getKeyType() == KeyType.ArrowLeft){
+            x-=1;
+        }
+        if (key.getKeyType() == KeyType.ArrowRight){
+            x+=1;
+        }
+
+        if (key.getKeyType() == KeyType.Character && key.getCharacter()
+                == 'q'){
+            screen.close();
+
+            System.exit(0);
+        }
+        return res;
+    }
     public void run() throws IOException {
-        draw();
+        boolean loop=true;
+        while (loop){
+            draw();
+            KeyStroke key = screen.readInput();
+            if( !processKey(key)){
+                loop=false;
+            }
+            draw();
+
+        }
 
     }
 }
